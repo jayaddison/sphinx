4305d11facf0c0c088e4b27794f524344e6221f2
James Addison <jay@jp-hosting.net>
2023-04-18 18:35:27 +0100

    Unit tests: linkcheck: configure a single linkcheck worker during 'test_defaults' test case
    With thanks to @picnixz for mentioning this configuration setting: https://github.com/sphinx-doc/sphinx/issues/11324#issuecomment-1511193722

--- tests/test_build_linkcheck.py
+++ tests/test_build_linkcheck.py
@@ -99,7 +99,7 @@ def test_defaults(app):
     with http_server(DefaultsHandler):
         with ConnectionMeasurement() as m:
             app.build()
-        assert m.connection_count == 5
+        assert m.connection_count == 1
 
     # Text output
     assert (app.outdir / 'output.txt').exists()
